5c051a82e28c78452582299f333db18b997d36ef
Andrew Magnuson <ajwm8103@users.noreply.github.com>
2023-03-11 18:56:52 -0500

    Small fixes

--- Assets/Scripts/Fitness/SwimmingFitness.cs
+++ Assets/Scripts/Fitness/SwimmingFitness.cs
@@ -10,6 +10,7 @@ public class SwimmingFitness : Fitness
     float distance, prevSpeed;
     Vector3 currCom, prevCom;
     Creature creature;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,22 +29,22 @@ public class SwimmingFitness : Fitness
         //Creature creature = myEnvironment.currentCreature;
         float reward = 0f;
 	
-	prevCom = currCom;
-       	currCom = creature.GetCentreOfMass();
-	prevSpeed = currSpeed;
-       	distance = Vector3.Distance(currCom,prevCom);
+	    prevCom = currCom;
+        currCom = creature.GetCentreOfMass();
+	    prevSpeed = currSpeed;
+        distance = Vector3.Distance(currCom,prevCom);
 
-	currSpeed = distance/Time.deltaTime;
-	reward += currSpeed;
+	    currSpeed = distance/Time.deltaTime;
+	    reward += currSpeed;
 	
-	// Continuing movement is rewarded over that from a single initial push, by giving the velocities during the final phase of the test period a stronger relative weight in the total fitness value
-	// We do not implement this because I am lazy
-	// Initial push <=> curr speed would be way slower than prev speed => apply discount to reward
+	    // Continuing movement is rewarded over that from a single initial push, by giving the velocities during the final phase of the test period a stronger relative weight in the total fitness value
+	    // We do not implement this because I am lazy
+	    // Initial push <=> curr speed would be way slower than prev speed => apply discount to reward
 
-	if(2*currSpeed < prevSpeed)
-	{
-		reward *= pushPenaltyDiscount;
-	}
+	    if(2 * currSpeed < prevSpeed)
+	    {
+		    reward *= pushPenaltyDiscount;
+	    }
 
 
         // figure out where the origin of the environemnt is (likely myEnvironment.transform.position)
--- Assets/Scripts/Fitness/WalkingFitness.cs
+++ Assets/Scripts/Fitness/WalkingFitness.cs
@@ -6,13 +6,17 @@ public class WalkingFitness : Fitness
 {
     public float pushThreshold = 2f;
     public float pushPenaltyDiscount = 0.9f;
+    Vector3 currCom;
+    Vector3 prevCom;
+    float distance, prevSpeed;
+    float currSpeed = 0f;
+    Creature creature;
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 currCom = creature.GetCentreOfMass();
-	Vector3 prevCom;
-	float distance, prevSpeed;
-	float currSpeed = 0f;
+        creature = myEnvironment.currentCreature;
+        currCom = creature.GetCentreOfMass();
     }
 
     // Update is called once per frame
@@ -26,22 +30,22 @@ public class WalkingFitness : Fitness
         Creature creature = myEnvironment.currentCreature;
         float reward = 0f;
 	
-	prevCom = currCom;
+	    prevCom = currCom;
        	currCom = creature.GetCentreOfMass();
-	prevSpeed = currSpeed;
+	    prevSpeed = currSpeed;
        	distance = Vector3.Distance(currCom,prevCom);
 
-	currSpeed = distance/Time.deltaTime;
-	reward += currSpeed;
+	    currSpeed = distance/Time.deltaTime;
+	    reward += currSpeed;
 	
-	// Continuing movement is rewarded over that from a single initial push, by giving the velocities during the final phase of the test period a stronger relative weight in the total fitness value
-	// We do not implement this because I am lazy
-	// Initial push <=> curr speed would be way slower than prev speed => apply discount to reward
+	    // Continuing movement is rewarded over that from a single initial push, by giving the velocities during the final phase of the test period a stronger relative weight in the total fitness value
+	    // We do not implement this because I am lazy
+	    // Initial push <=> curr speed would be way slower than prev speed => apply discount to reward
 
-	if(2*currSpeed < prevSpeed)
-	{
-		reward *= pushPenaltyDiscount;
-	}
+	    if(2 * currSpeed < prevSpeed)
+	    {
+		    reward *= pushPenaltyDiscount;
+	    }
 
         return reward;
     }
