6da3076c9631496a066145d4bc201e4a292b1d64
Andrew Magnuson <ajwm8103@users.noreply.github.com>
2023-03-11 18:09:22 -0500

    Changed filename

--- Assets/Scripts/Training/KSSAlgorithm.cs
+++ Assets/Scripts/Training/KSSAlgorithm.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EvolutionAlgorithm : TrainingAlgorithm
+[System.Serializable]
+public class KSSSave {
+    
+}
+
+
+public class KSSAlgorithm : TrainingAlgorithm
 {
     // Start is called before the first frame update
     void Start()
