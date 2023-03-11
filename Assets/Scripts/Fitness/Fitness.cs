42fe444555ce919482d8e33dfa0965a31c58688c
latexsupremecist <dan.chua@mail.utoronto.ca>
2023-03-11 16:35:28 -0500

    changed fitness to interface

--- Assets/Scripts/Fitness/Fitness.cs
+++ Assets/Scripts/Fitness/Fitness.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fitness : MonoBehaviour
+interface IFitness
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Fitness/SwimmingFitness.cs
+++ Assets/Scripts/Fitness/SwimmingFitness.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwimmingFitness : MonoBehaviour
+public class SwimmingFitness : MonoBehaviour : IFitness
 {
     // Start is called before the first frame update
     void Start()
