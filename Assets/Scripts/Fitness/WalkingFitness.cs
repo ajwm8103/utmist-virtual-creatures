56e13ab50c8d46ed3f5e648ce1d104b454b3bcb9
latexsupremecist <dan.chua@mail.utoronto.ca>
2023-03-11 18:10:11 -0500

    editing to follow style guide

--- Assets/Scripts/Fitness/WalkingFitness.cs
+++ Assets/Scripts/Fitness/WalkingFitness.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class WalkingFitness : Fitness
 {
-    public float push_threshold = 2f;
-    public float push_penalty_discount = 0.9f;
+    public float pushThreshold = 2f;
+    public float pushPenaltyDiscount = 0.9f;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 curr_com = creature.GetCentreOfMass();
-	Vector3 prev_com;
-	float distance, prev_speed;
-	float curr_speed = 0f;
+        Vector3 currCom = creature.GetCentreOfMass();
+	Vector3 prevCom;
+	float distance, prevSpeed;
+	float currSpeed = 0f;
     }
 
     // Update is called once per frame
@@ -26,21 +26,21 @@ public class WalkingFitness : Fitness
         Creature creature = myEnvironment.currentCreature;
         float reward = 0f;
 	
-	prev_com = curr_com;
+	prev_com = currCom;
        	curr_com = creature.GetCentreOfMass();
-	prev_speed = curr_speed;
-       	distance = Vector3.Distance(curr_com,prev_com);
+	prev_speed = currSpeed;
+       	distance = Vector3.Distance(currCom,prevCom);
 
 	curr_speed = distance/Time.deltaTime;
-	reward += curr_speed;
+	reward += currSpeed;
 	
 	// Continuing movement is rewarded over that from a single initial push, by giving the velocities during the final phase of the test period a stronger relative weight in the total fitness value
 	// We do not implement this because I am lazy
 	// Initial push <=> curr speed would be way slower than prev speed => apply discount to reward
 
-	if(2*curr_speed < prev_speed)
+	if(2*currSpeed < prevSpeed)
 	{
-		reward *= push_penalty_discount;
+		reward *= pushPenaltyDiscount;
 	}
 
         return reward;
