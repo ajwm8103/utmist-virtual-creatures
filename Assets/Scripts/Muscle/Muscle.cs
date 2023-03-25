6e99d6f88e9dafed001a09b3d01222a695d7260c
latexsupremecist <dan.chua@mail.utoronto.ca>
2023-03-25 17:45:48 -0400

    formatting

--- Assets/Scripts/Muscle/Muscle.cs
+++ Assets/Scripts/Muscle/Muscle.cs
@@ -9,15 +9,15 @@ public class MusclePreferenceSetting
     public float segmentLength = 0.5f; // l_s
     public float momentArm = 0.04f; // d
     public float refLength = 0.5f; // l_ref
-    public float refAngle = 110*Math.PI/180; // phi_ref
+    public float refAngle = 110 * Math.PI / 180; // phi_ref
     public float maxForce = 22000f; // F_max
     public float optLength 0.1f; // l_opt
-    public float width = 0.4*optLength; // w
-    public float maxVelocity = -12*optLength; //v_max
+    public float width = 0.4 * optLength; // w
+    public float maxVelocity = -12 * optLength; //v_max
     public float eccentricForceEnhancement = 1.5f; // N
     public float curvatureConst = 5f; // K
     public float restLength = 0.4f; // l_ref
-    public float refStrain = 0.04f; // epsilon_ref
+    public float refStrain = 0.04 * restLength; // epsilon_ref
     public float couplingConst = 0.01f; // tau
     public float timeDelay = 0.015f; // delta_P
 }
@@ -51,10 +51,9 @@ public class Muscle
     public float ForceSEE(lengthSEE, mps) // Serial Elastic Element
     {
         float epsilon = (lengthSEE - mps.restLength)/mps.restLength;
-        float epsilonRef = 0.04f;
         if (epsilon > 0)
         {
-            return (epsilon/epsilonRef)^2;
+            return (epsilon/mps.refStrain)^2;
         }
         return 0f;
     }
